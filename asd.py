40684a99fe1907fa26e83e2bb88a866e7a0ca8d4
User <vitalikvonsul@gmail.com>
2024-09-25 23:40:44 +1200

    asdasda

--- asd.py
+++ asd.py
@@ -1,9 +1,11 @@
-x = int(input('Введите сумму вклада: '))
-p = int(input('Введите процент: '))
-y = int(input('Введите сумму цели: '))
-years = 0
-while x < y:
-    x *= 1 + p / 100
-    x = int(100 * x) / 100
-    years += 1
-print(f'Понадобится {years} лет.')
+num1 = int(input('Введите сумму вклада: '))
+num2 = float(input('Введите процент: '))
+num3 = int(input('Введите сумму цели: '))
+num4 = num1 + num2 + num3
+i = 0
+print(round(num4, 0))
+while num1 < num3:
+    num1 *= 1 + num2 / 100
+    num1 = int(100 * num1) / 100
+    i += 1
+    print(f'Понадобится {i} лет.')
